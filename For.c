497a9a6bd37dcb09f0fda91bfe951873cdecd902
Zhenkun Guo <1608229093@qq.com>
2021-11-28 22:13:24 +0800

    for

---
+++ For.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+	int i = 0;
+	//  初始化   判 断   调整
+	for (i = 1; i <= 10; i++)
+	{
+		printf("%d ", i);
+		if (i = 5)
+			continue;
+			/*break;*/
+	}
+	return 0;
+}
